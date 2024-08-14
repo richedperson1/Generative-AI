4a89a74b109c24d979fca737eab8a64109a2f7f4
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-08-14 13:47:53 +0530

    adding the graph for the langchain

---
+++ test.py
@@ -0,0 +1,8 @@
+**Joke Title:** "Tandoori Chicken Ka Kissa"
+
+**Joke:** Ek din, ek Pakistani chef ne apne dukaan se Tandoori chicken ka order le liya. Par jab woh uska bill diya to usne kaha, "Bhai! Yeh 500 rupees ke saath hi nahi chal sakta, maine toh ismein Chicken karwa ke ja raha tha!"
+
+**Translation:** One day, a Pakistani chef took an order for Tandoori chicken from his shop. But when he gave him the bill, he said, "Brother! This can't be 500 rupees alone, I had to make the chicken run inside!"
+
+I hope this joke made you smile!
+
